e20dd25f1b81e193a3df579732636d2f38b3308e
MindIssawara <110963945+MindIssawara@users.noreply.github.com>
2025-02-21 02:00:14 +0700

    Update ex23.hs

--- ex23.hs
+++ ex23.hs
@@ -1,6 +1,9 @@
-import System.Random
 -- can you reuse code from the previous version?
---  Yes, I reused randNum, verdict', inRange, getRange and readGuess from Aj. Chin's version. I modified readNumber runGameRg and v5(from V4).
+--  Yes, I reused from Aj. Chin's version and I modified some.
+
+
+
+import System.Random
 
 -- try to separate your code into pure and impure parts
 -- pure parts
@@ -16,7 +19,7 @@ instance Functor (Reader r) where
 
 instance Applicative (Reader r) where
   pure x = Reader $ const x
-  (Reader rf) <*> (Reader rx) = Reader $ rf <*> rx
+  (Reader rf) <*> (Reader rx) = Reader $ \r -> rf r (rx r)
 
 instance Monad (Reader r) where
   (Reader ra) >>= f = Reader $ \r -> runReader (f (ra r)) r
@@ -41,62 +44,66 @@ instance Monoid w => Monad (Writer w) where
 randNum :: RandomGen g => g -> Int
 randNum gen = fst $ uniform gen
 
-readNumber :: [Char] -> IO Int
+readNumber :: String -> IO Int
 readNumber msg = do
     putStr $ msg ++ ": "
     line <- getLine
     case reads line of
-        [(n, "")] -> return n
-        _ -> do
-            putStrLn "Invalid input. Please enter a number."
-            readNumber msg
+      [(n, "")] -> return n
+      _ -> do
+         putStrLn "Invalid input. Please enter a number."
+         readNumber msg
 
 verdict' :: Ord a => a -> a -> (Maybe a, Maybe a) -> Either (String, (Maybe a, Maybe a)) String
 verdict' target guess (lo, hi) =
-    case compare guess target of
-        EQ -> Right "You win!"
-        LT -> Left ("Too low", (Just guess, hi))
-        GT -> Left ("Too high", (lo, Just guess))
+  case compare guess target of
+     EQ -> Right "You win!"
+     LT -> Left ("Too low", (Just guess, hi))
+     GT -> Left ("Too high", (lo, Just guess))
 
 inRange :: Ord a => (Maybe a, Maybe a) -> a -> Bool
 inRange (lo, hi) guess =
-    maybe True (<guess) lo && maybe True (>guess) hi
+  maybe True (< guess) lo && maybe True (> guess) hi
 
--- impure parts
 getRange :: IO (Int, Int)
 getRange = do
-    lo <- readNumber "Lower bound"
-    hi <- readNumber "Upper bound"
-    if lo > hi
-    then do
-        putStrLn "Invalid range"
-        getRange
-    else return (lo, hi)
+  lo <- readNumber "Lower bound"
+  hi <- readNumber "Upper bound"
+  if lo > hi then do
+    putStrLn "Invalid range"
+    getRange
+  else return (lo, hi)
 
+-- impure parts
 readGuess :: (Maybe Int, Maybe Int) -> IO Int
 readGuess range = do
-    guess <- readNumber "Guess"
-    if inRange range guess
+  guess <- readNumber "Guess"
+  if inRange range guess
     then return guess
     else do
-        putStrLn "Impossible answer"
-        readGuess range
+      putStrLn "Impossible answer"
+      readGuess range
 
-runGameRg :: Int -> (Maybe Int, Maybe Int) -> Int -> (Int -> Bool) -> GuessLog -> IO ()
-runGameRg num range count cont guesses = do
+combineWriter :: Writer GuessLog () -> Writer GuessLog () -> Writer GuessLog ()
+combineWriter (Writer ((), log1)) (Writer ((), log2)) = Writer ((), log1 <> log2)
+
+runGameRg :: Int -> (Maybe Int, Maybe Int) -> Int -> Reader Int (IO (Writer GuessLog ()))
+runGameRg num range count = Reader $ \lim -> do
     guess <- readGuess range
     let v = verdict' num guess range
     case v of
-        Right msg -> do
-            putStrLn msg
-            putStrLn $ "Numbers guessed: " ++ unwords (map show (reverse (guess : guesses)))
-        Left (msg, range') -> do
-            putStrLn msg
-            if cont count
-            then runGameRg num range' (count + 1) cont (guess : guesses)
-            else do
-                putStrLn "Game over"
-                putStrLn $ "Numbers guessed: " ++ concatMap (++ ", ") (map show (guess : guesses))
+      Right msg -> do
+         putStrLn msg
+         return (Writer ((), [guess]))
+      Left (msg, newRange) -> do
+         putStrLn msg
+         if count < lim then do
+            next <- runReader (runGameRg num newRange (count + 1)) lim
+            let current = Writer ((), [guess])
+            return (combineWriter current next)
+         else do
+            putStrLn "Game over"
+            return (Writer ((), [guess]))
 
 v5 :: IO ()
 v5 = do
@@ -104,6 +111,6 @@ v5 = do
     range <- getRange
     lim <- readNumber "Guess limit"
     let (num, _) = uniformR range g
-    runGameRg num (Nothing, Nothing) 1 (<lim) []
-
-
+    writerResult <- runReader (runGameRg num (Nothing, Nothing) 1) lim
+    let (_, log) = runWriter writerResult
+    putStrLn $ "Numbers guessed: " ++ unwords (map show log)
